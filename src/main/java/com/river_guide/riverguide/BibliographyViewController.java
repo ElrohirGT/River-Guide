e16429a80b8b1bb1b4d029533f9e27a2e0f093a6
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2022-11-02 20:47:32 -0600

    Se han añadido nuevas referencias bibliográficas.

--- src/main/java/com/river_guide/riverguide/BibliographyViewController.java
+++ src/main/java/com/river_guide/riverguide/BibliographyViewController.java
@@ -32,6 +32,28 @@ public class BibliographyViewController implements Initializable {
                     "https://amuyshondtdotcom.wordpress.com/2016/05/11/rio-paz-se-encuentra-contaminado-con-melaza-publicacion-tomada-de-prensa-libre-guatemala/");
             put("11 de los 25 ríos más contaminantes de Centroamérica están en Guatemala",
                     "https://revistaviatori.com/notas/11-de-25-rios-contaminantes-guatemala/");
+            put("Acciones contribuyen a saneamiento del río Motagua", 
+                    "https://dca.gob.gt/noticias-guatemala-diario-centro-america/acciones-contribuyen-a-saneamiento-del-rio-motagua/");
+            put("MARN promueve acciones para proteger el río Salamá", 
+                    "https://agn.gt/marn-impulsa-acciones-para-proteger-el-rio-samala/");
+            put("Colocarán cercas en río Las Vacas para recolectar desechos sólidos", 
+                    "https://agn.gt/colocaran-cercas-en-rio-las-vacas-para-recolectar-desechos-solidos/");
+            put("La Pasión: Desastre ecológico y social", 
+                    "https://cmiguate.org/la-pasion-desastre-ecologico-y-social/");
+            put("Río Dulce: Una joya natural a las puertas del Caribe", 
+                    "https://www.visitcentroamerica.com/visitar/rio-dulce/");
+            put("Río Coyolate recibe 2,500 peces nativos", 
+                    "https://icc.org.gt/es/rio-coyolate-recibe-2500-peces-nativos/");
+            put("Ministros de Ambiente de El Salvador y Guatemala se reúnen para coordinar acciones en Cuenca del Río Paz", 
+                    "https://www.sica.int/busqueda/Noticias.aspx?IDItem=14065&IDCat=3&IdEnt=2&Idm=1&IdmStyle=1");
+            put("MARN reforesta cuenca baja del río Los Esclavos en Santa Rosa", 
+                    "https://www.marn.gob.gt/marn-reforesta-cuenca-baja-del-rio-los-esclavos-en-santa-rosa/");
+            put("Comité acciona para salvar el Río Cahabón", 
+                    "https://www.sica.int/busqueda/Noticias.aspx?IDItem=46048&IDCat=2&IdEnt=47");
+            put("Ejecutan acciones para el cuidado de la cuenca del río Naranjo", 
+                    "https://agn.gt/ejecutan-acciones-para-el-cuidado-de-la-cuenca-del-rio-naranjo/");
+            put("Recolectan desechos sólidos en la ribera del río Suchiate", 
+                    "https://www.marn.gob.gt/recolectan-desechos-solidos-en-la-ribera-del-rio-suchiate/");
         }
     };
     private HostServices _hostServices;
