162c23ac6d63d85d5c079ecf9fa87824eff42b8a
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2022-11-15 09:55:04 -0600

    Conexión online de la base de datos.

--- src/main/java/com/river_guide/riverguide/ConnectDB.java
+++ src/main/java/com/river_guide/riverguide/ConnectDB.java
@@ -7,7 +7,8 @@ public class ConnectDB {
         //Connection conn= null;
         try{
             Class.forName("org.mariadb.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/rios","Grupo3","POO2022");
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://sql734.main-hosting.eu/u991565456_riverguide","u991565456_grupo3","Grupo.3POO");
+            //Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/rios","Grupo3","POO2022");
             System.out.println("Connection Success");
             return conn;
         }catch(Exception e){
