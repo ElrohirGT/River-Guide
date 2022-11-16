a56bc67179cabd3b393ae88c44217eacb9fff570
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2022-11-16 15:30:31 -0600

    Update ConnectDB.java

--- src/main/java/com/river_guide/riverguide/ConnectDB.java
+++ src/main/java/com/river_guide/riverguide/ConnectDB.java
@@ -6,8 +6,13 @@ public class ConnectDB {
     public static Connection ConnectMariaDB() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
+            
+            /* Para conectar con la base de datos en el hosting */ 
             Connection conn = DriverManager.getConnection("jdbc:mariadb://sql734.main-hosting.eu/u991565456_riverguide",
                     "u991565456_grupo3", "Grupo.3POO");
+            
+            
+            /* Para conectar a la base de datos de manera local */
             // Connection conn =
             // DriverManager.getConnection("jdbc:mariadb://localhost/rios", "Grupo3",
             // "POO2022");
