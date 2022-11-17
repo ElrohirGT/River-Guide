8e03abc8eaa8408e25ab472a490fd994ee419621
ElrohirGT <elrohirgt@gmail.com>
2022-11-17 08:38:42 -0600

    Changed the connection

--- src/main/java/com/river_guide/riverguide/ConnectDB.java
+++ src/main/java/com/river_guide/riverguide/ConnectDB.java
@@ -8,13 +8,13 @@ public class ConnectDB {
             Class.forName("org.mariadb.jdbc.Driver");
 
             /* Para conectar con la base de datos en el hosting */
-            // Connection conn =
-            // DriverManager.getConnection("jdbc:mariadb://sql734.main-hosting.eu/u991565456_riverguide",
-            // "u991565456_grupo3", "Grupo.3POO");
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://sql734.main-hosting.eu/u991565456_riverguide",
+                    "u991565456_grupo3", "Grupo.3POO");
 
             /* Para conectar a la base de datos de manera local */
-            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/rios", "Grupo3",
-                    "POO2022");
+            // Connection conn =
+            // DriverManager.getConnection("jdbc:mariadb://localhost/rios", "Grupo3",
+            // "POO2022");
             System.out.println("Connection Success");
             return conn;
         } catch (Exception e) {
